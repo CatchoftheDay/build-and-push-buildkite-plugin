954975a38e23627b24abfe7874f9bda550bc40df
Dylan Smith <9061024+Chemsmith@users.noreply.github.com>
2023-03-20 14:29:07 +1100

    Fix tests after refactor

--- pipeline/tests.py
+++ pipeline/tests.py
@@ -1,7 +1,7 @@
 import os
 from unittest import mock, main, TestCase
 
-from pipeline import create_build_step, create_manifest_step, create_scan_step, process_env_to_config, PLUGIN_ENV_PREFIX
+from pipeline import create_build_step, create_oci_manifest_step, create_scan_step, process_env_to_config, PLUGIN_ENV_PREFIX
 
 
 class TestPipelineGeneration(TestCase):
@@ -42,18 +42,17 @@ class TestPipelineGeneration(TestCase):
         this.assertEqual(step['label'], ':docker: Build and push arm image')
         this.assertEqual(step['agents'], {'queue': 'docker-arm'})
         this.assertEqual(step['command'], [
-            'docker buildx build --push --ssh default=$$SSH_AUTH_SOCK --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890-arm -f Dockerfile .',
+            'docker buildx build --push --ssh default --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
         ])
         this.assertEqual(step['env'], {'DOCKER_BUILDKIT': '1'})
         this.assertEqual(step['key'], 'build-push-arm')
 
     def test_create_manifest_step(this):
-        step = create_manifest_step(this.config)
+        step = create_oci_manifest_step(this.config)
 
         this.assertEqual(step['command'], [
-            'docker manifest create 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890-arm',
+            'docker manifest create 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm',
             'docker manifest push 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890',
-            'aws ecr batch-delete-image --registry-id 362995399210 --repository-name catch/testcase --image-ids TAG="1234567890-arm"',
         ])
 
         this.assertEqual(step['depends_on'], ['build-push-arm'])
@@ -64,13 +63,11 @@ class TestPipelineGeneration(TestCase):
         multi_arch_config = this.config.copy()
         multi_arch_config['build_x86'] = True
 
-        step = create_manifest_step(multi_arch_config)
+        step = create_oci_manifest_step(multi_arch_config)
 
         this.assertEqual(step['command'], [
-            'docker manifest create 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890-x86',
+            'docker manifest create 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
             'docker manifest push 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890',
-            'aws ecr batch-delete-image --registry-id 362995399210 --repository-name catch/testcase --image-ids TAG="1234567890-arm"',
-            'aws ecr batch-delete-image --registry-id 362995399210 --repository-name catch/testcase --image-ids TAG="1234567890-x86"',
         ])
 
         this.assertEqual(step['depends_on'], ['build-push-arm', 'build-push-x86'])
@@ -83,7 +80,7 @@ class TestPipelineGeneration(TestCase):
         this.assertEqual(step['command'], [
             'if [[ -z "$${RAPID7_API_KEY}" ]]; then echo "A Rapid7 API key needs to be added to your build secrets as RAPID7_API_KEY"; exit 1; fi',
             'docker save 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 -o "testcase.tar"',
-            'docker run --rm -v $(pwd)/docker_image.tar:/docker_image.tar rapid7/container-image-scanner:latest -f=/docker_image.tar -k=$$RAPID7_API_KEY -r=au --buildId "1234567890" --buildName testcase',
+            'docker run --rm -v $(pwd)/testcase.tar:/testcase.tar rapid7/container-image-scanner:latest -f=/testcase.tar -k=$$RAPID7_API_KEY -r=au --buildId "1234567890" --buildName testcase',
         ])
 
         this.assertEqual(step['depends_on'], 'create-container-manifest')
