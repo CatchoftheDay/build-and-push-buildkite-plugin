08b5926b5fba2d0c230a74761cc4909a42dff8bc
Dylan Smith <9061024+Chemsmith@users.noreply.github.com>
2023-09-22 13:28:14 +1000

    test: add tests for tagged runs

--- pipeline/tests.py
+++ pipeline/tests.py
@@ -1,7 +1,7 @@
 import os
 from unittest import mock, main, TestCase
 
-from pipeline import create_build_step, create_oci_manifest_step, process_env_to_config, PLUGIN_ENV_PREFIX, CURRENT_BRANCH, BUILDKIT_VERSION
+from pipeline import create_build_step, create_oci_manifest_step, process_env_to_config, PLUGIN_ENV_PREFIX, BUILDKIT_VERSION
 
 
 class TestPipelineGeneration(TestCase):
@@ -41,12 +41,16 @@ class TestPipelineGeneration(TestCase):
     }
 
     @mock.patch.dict(os.environ, RUNTIME_ENVS)
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_process_env_to_config(this):
         config = process_env_to_config()
 
         this.assertEqual(config, this.config)
 
     @mock.patch.dict(os.environ, dict({f'{PLUGIN_ENV_PREFIX}PUSH_BRANCHES': 'testing'}, **RUNTIME_ENVS))
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_process_env_to_config_no_push_branch(this):
         config = process_env_to_config()
 
@@ -57,6 +61,8 @@ class TestPipelineGeneration(TestCase):
         this.assertEqual(config, no_config)
 
     @mock.patch.dict(os.environ, dict({f'{PLUGIN_ENV_PREFIX}PUSH_BRANCHES': 'testing,main'}, **RUNTIME_ENVS))
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_process_env_to_config_no_push_branch(this):
         config = process_env_to_config()
 
@@ -67,6 +73,8 @@ class TestPipelineGeneration(TestCase):
 
 
     @mock.patch.dict(os.environ, RUNTIME_ENVS)
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_create_build_step_push(this):
         platform = 'arm'
         agent = 'docker-arm'
@@ -78,7 +86,34 @@ class TestPipelineGeneration(TestCase):
         this.maxDiff = None
         this.assertEqual(step['command'], [
             f'docker buildx use builder || docker buildx create --bootstrap --name builder --use --driver docker-container --driver-opt image=moby/buildkit:{BUILDKIT_VERSION}',
-            f'docker buildx build --load --pull --ssh default  --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:master --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:{CURRENT_BRANCH} --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN    --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
+            f'docker buildx build --load --pull --ssh default  --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:master --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN    --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
+            'curl -o wizcli https://wizcli.app.wiz.io/latest/wizcli-linux-arm64',
+            'chmod +x ./wizcli',
+            './wizcli auth --id $$WIZ_CLIENT_ID --secret $$WIZ_CLIENT_SECRET',
+            './wizcli docker scan --image 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -p "Container Scanning" -p "Secret Scanning" --tag pipeline=testcase --tag architecture=arm --tag pipeline_run=110 > out 2>&1 | true; SCAN_STATUS=0',
+            'if [[ $$SCAN_STATUS -eq 0 ]]; then echo -e "**Container scan report (arm)**\n\n<details><summary></summary>\n\n\\`\\`\\`term\n$(cat out)\\`\\`\\`\n\n</details>" | buildkite-agent annotate --style info --context arm-image-security-scan; else echo -e "**Container scan report (arm)**\n\n<details><summary></summary>\n\n\\`\\`\\`term\n$(cat out**)\\`\\`\\`\n\n</details>" | buildkite-agent annotate --style error --context arm-image-security-scan; fi',
+            'test $$SCAN_STATUS -eq 0 || exit 1',
+            'docker image push 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm'
+        ])
+
+        this.assertEqual(step['env'], {'DOCKER_BUILDKIT': '1'})
+        this.assertEqual(step['key'], 'build-and-push-build-push-arm')
+    
+    @mock.patch.dict(os.environ, dict({f'BUILDKITE_TAG': 'v1.0.0', 'BUILDKITE_BRANCH': 'v1.0.0'}, **RUNTIME_ENVS))
+    @mock.patch('pipeline.CURRENT_BRANCH', 'v1.0.0')
+    @mock.patch('pipeline.CURRENT_TAG', 'v1.0.0')
+    def test_create_build_step_push_tags(this):
+        platform = 'arm'
+        agent = 'docker-arm'
+
+        step = create_build_step(platform, agent, this.config)
+
+        this.assertEqual(step['label'], ':docker: Build and push arm image')
+        this.assertEqual(step['agents'], {'queue': 'docker-arm'})
+        this.maxDiff = None
+        this.assertEqual(step['command'], [
+            f'docker buildx use builder || docker buildx create --bootstrap --name builder --use --driver docker-container --driver-opt image=moby/buildkit:{BUILDKIT_VERSION}',
+            f'docker buildx build --load --pull --ssh default  --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:master --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:v1.0.0 --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN    --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
             'curl -o wizcli https://wizcli.app.wiz.io/latest/wizcli-linux-arm64',
             'chmod +x ./wizcli',
             './wizcli auth --id $$WIZ_CLIENT_ID --secret $$WIZ_CLIENT_SECRET',
@@ -92,9 +127,12 @@ class TestPipelineGeneration(TestCase):
         this.assertEqual(step['key'], 'build-and-push-build-push-arm')
 
     @mock.patch.dict(os.environ, RUNTIME_ENVS)
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_create_build_step_no_push(this):
         platform = 'arm'
         agent = 'docker-arm'
+        
         config = this.config.copy()
         config['push_to_ecr'] = False
         step = create_build_step(platform, agent, config)
@@ -104,7 +142,7 @@ class TestPipelineGeneration(TestCase):
         this.maxDiff = None
         this.assertEqual(step['command'], [
             f'docker buildx use builder || docker buildx create --bootstrap --name builder --use --driver docker-container --driver-opt image=moby/buildkit:{BUILDKIT_VERSION}',
-            f'docker buildx build --load --pull --ssh default  --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:master --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:{CURRENT_BRANCH} --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN    --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
+            f'docker buildx build --load --pull --ssh default  --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main --cache-from type=registry,ref=362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:master --build-arg arg1=42 --build-arg arg2 --build-arg GITHUB_TOKEN    --tag 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm -f Dockerfile .',
             'curl -o wizcli https://wizcli.app.wiz.io/latest/wizcli-linux-arm64',
             'chmod +x ./wizcli',
             './wizcli auth --id $$WIZ_CLIENT_ID --secret $$WIZ_CLIENT_SECRET',
@@ -116,13 +154,15 @@ class TestPipelineGeneration(TestCase):
 
         this.assertEqual(step['env'], {'DOCKER_BUILDKIT': '1'})
         this.assertEqual(step['key'], 'build-and-push-build-push-arm')
-
+    
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_create_manifest_step(this):
         step = create_oci_manifest_step(this.config)
 
         this.assertEqual(step['command'], [
             'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm',
-            f'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:{CURRENT_BRANCH} 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm',
+            f'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm',
         ])
 
         this.assertEqual(step['depends_on'], ['build-and-push-build-push-arm'])
@@ -130,6 +170,8 @@ class TestPipelineGeneration(TestCase):
         this.assertNotIn('agent', step)
 
     @mock.patch.dict(os.environ, RUNTIME_ENVS)
+    @mock.patch('pipeline.CURRENT_BRANCH', 'main')
+    @mock.patch('pipeline.CURRENT_TAG', '')
     def test_create_manifest_step_multi_arch(this):
         multi_arch_config = this.config.copy()
         multi_arch_config['build_x86'] = True
@@ -138,7 +180,25 @@ class TestPipelineGeneration(TestCase):
 
         this.assertEqual(step['command'], [
             'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
-            f'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:{CURRENT_BRANCH} 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
+            f'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:main 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
+        ])
+
+        this.assertEqual(step['depends_on'], ['build-and-push-build-push-arm', 'build-and-push-build-push-x86'])
+
+        this.assertNotIn('agent', step)
+
+    @mock.patch.dict(os.environ, dict({f'BUILDKITE_TAG': 'v1.0.0', 'BUILDKITE_BRANCH': 'v1.0.0'}, **RUNTIME_ENVS))
+    @mock.patch('pipeline.CURRENT_BRANCH', 'v1.0.0')
+    @mock.patch('pipeline.CURRENT_TAG', 'v1.0.0')
+    def test_create_manifest_step_multi_arch_tag(this):
+        multi_arch_config = this.config.copy()
+        multi_arch_config['build_x86'] = True
+
+        step = create_oci_manifest_step(multi_arch_config)
+
+        this.assertEqual(step['command'], [
+            'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:1234567890 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
+            f'docker buildx imagetools create -t 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:v1.0.0 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-arm 362995399210.dkr.ecr.ap-southeast-2.amazonaws.com/catch/testcase:multi-platform-1234567890-x86',
         ])
 
         this.assertEqual(step['depends_on'], ['build-and-push-build-push-arm', 'build-and-push-build-push-x86'])
