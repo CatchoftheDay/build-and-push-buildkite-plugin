63ce52ee743e43bdcb592c50b5cef592c0a08d99
Dylan Smith <9061024+Chemsmith@users.noreply.github.com>
2024-03-19 16:10:36 +1100

    refactor: use BUILDKITE_PLUGIN_CONFIGURATION rather than seperate envvars for configuration

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,4 +1,5 @@
 """A Buildkite plugin to build and push container images to ECR"""
+import json
 import os
 import time
 
@@ -15,7 +16,6 @@ CURRENT_BRANCH: str = os.environ.get("BUILDKITE_BRANCH", "")
 CURRENT_TAG: str = os.environ.get("BUILDKITE_TAG", "")
 
 PLUGIN_NAME: str = "build-and-push"
-PLUGIN_ENV_PREFIX: str = f"BUILDKITE_PLUGIN_{PLUGIN_NAME.upper().replace('-', '_')}_"
 
 BUILD_PLATFORMS: Dict[str, str] = {
     # platform => buildkite agent name
@@ -30,80 +30,78 @@ BLOCK_ON_CONTAINER_SCAN = (
 UNIX_BUILD_TIME = int(time.time())
 
 
-def process_env_to_config() -> Dict[str, Any]:
+def process_config() -> Dict[str, Any]:
     """Process buildkite plugin environment variables into a config dict"""
     config_definition: Dict[str, Any] = {
-        "dockerfile_path": {
+        "dockerfile-path": {
             "type": "string",
             "default": "Dockerfile",
         },
-        "context_path": {
+        "context-path": {
             "type": "string",
             "default": ".",
         },
-        "image_name": {
+        "image-name": {
             "type": "string",
             "default": os.environ["BUILDKITE_PIPELINE_NAME"],
         },
-        "image_tag": {
+        "image-tag": {
             "type": "string",
             "default": os.environ["BUILDKITE_COMMIT"][0:10],
         },
-        "additional_tag": {
+        "additional-tag": {
             "type": "string",
             "default": None,
         },
-        "build_args": {
+        "build-args": {
             "type": "list",
             "default": [],
         },
-        "build_arm": {
+        "build-arm": {
             "type": "bool",
             "default": True,
         },
-        "build_x86": {
+        "build-x86": {
             "type": "bool",
             "default": True,
         },
-        "scan_image": {
+        "scan-image": {
             "type": "bool",
             "default": True,
         },
-        "group_key": {
+        "group-key": {
             "type": "string",
             "default": "build-and-push",
         },
-        "always_pull": {
+        "always-pull": {
             "type": "bool",
             "default": True,
         },
-        "composer_cache": {
+        "composer-cache": {
             "type": "bool",
             "default": False,
         },
-        "npm_cache": {
+        "npm-cache": {
             "type": "bool",
             "default": False,
         },
-        "yarn_cache": {
+        "yarn-cache": {
             "type": "bool",
             "default": False,
         },
-        "push_branches": {
+        "push-branches": {
             "type": "list",
             "default": [],
         },
-        "repository_namespace": {
+        "repository-namespace": {
             "type": "string",
             "default": "catch",
         },
-        "mutate_image_tag": {
+        "mutate-image-tag": {
             "type": "bool",
             "default": False,
         },
     }
-
-    config = {}
 
     def process_bool(value: str) -> bool:
         return value.lower() == "true"
@@ -111,42 +109,39 @@ def process_env_to_config() -> Dict[str, Any]:
     def process_list(value: str) -> List[str]:
         return value.split(",")
 
-    for name, value in os.environ.items():
-        if name.startswith(PLUGIN_ENV_PREFIX):
-            key = name.replace(PLUGIN_ENV_PREFIX, "").lower()
+    if "BUILDKITE_PLUGIN_CONFIGURATION" not in os.environ:
+        print("BUILDKITE_PLUGIN_CONFIGURATION environment variable not set, assuming no plugin configuration has been provided")
 
-            if key not in config_definition:
-                continue
-
-            if config_definition[key]["type"] == "bool":
-                config[key] = process_bool(value)
-            elif config_definition[key]["type"] == "list":
-                config[key] = process_list(value)
-            else:
-                config[key] = value
+    config = json.loads(os.environ.get("BUILDKITE_PLUGIN_CONFIGURATION", "{}"))
 
     for name, value in config_definition.items():
         if name not in config:
             config[name] = value.get("default", None)
+            continue
 
-    config["build_args"].append("GITHUB_TOKEN")
-    config["build_args"].append("BUILDKITE_COMMIT")
-    config["build_args"].append("BUILDKITE_JOB_ID")
-    config["build_args"].append(f"BUILD_DATE={UNIX_BUILD_TIME}")
+        if value["type"] == "bool":
+            config[name] = process_bool(config[name])
+        elif value["type"] == "list" and isinstance(config[name], str):
+            config[name] = process_list(config[name])
 
-    config["group_key"] = sanitise_step_key(config["group_key"])
+    config["build-args"].append("GITHUB_TOKEN")
+    config["build-args"].append("BUILDKITE_COMMIT")
+    config["build-args"].append("BUILDKITE_JOB_ID")
+    config["build-args"].append(f"BUILD_DATE={UNIX_BUILD_TIME}")
+
+    config["group-key"] = sanitise_step_key(config["group-key"])
 
     ecr_repository_namespace_joiner = (
-        f'{config["repository_namespace"]}/' if config["repository_namespace"] else ""
+        f'{config["repository-namespace"]}/' if config["repository-namespace"] else ""
     )
 
     config[
-        "fully_qualified_image_name"
-    ] = f'{ECR_ACCOUNT}.dkr.ecr.{ECR_REGION}.amazonaws.com/{ecr_repository_namespace_joiner}{config["image_name"]}'
+        "fully-qualified-image-name"
+    ] = f'{ECR_ACCOUNT}.dkr.ecr.{ECR_REGION}.amazonaws.com/{ecr_repository_namespace_joiner}{config["image-name"]}'
 
-    config["push_to_ecr"] = (
-        not config["push_branches"]
-        or CURRENT_BRANCH in config["push_branches"]
+    config["push-to-ecr"] = (
+        not config["push-branches"]
+        or CURRENT_BRANCH in config["push-branches"]
         or CURRENT_BRANCH == CURRENT_TAG
     )
 
@@ -163,12 +158,12 @@ def create_build_step(
     platform: str, agent: str, config: Dict[str, Any]
 ) -> Dict[str, Any]:
     """Create a step stub to build and push a container image for a given platform"""
-    platform_image: str = f'{config["fully_qualified_image_name"]}:multi-platform-{config["image_tag"]}-{platform}'
+    platform_image: str = f'{config["fully-qualified-image-name"]}:multi-platform-{config["image-tag"]}-{platform}'
 
     cache_from_tags: List[str] = sorted(
         set(
             [
-                config["image_tag"],
+                config["image-tag"],
                 CURRENT_BRANCH,
                 "master",
                 "main",
@@ -177,44 +172,44 @@ def create_build_step(
     )
     cache_from_images_stub: str = "".join(
         [
-            f' --cache-from type=registry,ref={config["fully_qualified_image_name"]}:cache_{tag}'
+            f' --cache-from type=registry,ref={config["fully-qualified-image-name"]}:cache_{tag}'
             for tag in cache_from_tags
         ]
     )
 
     build_args: str = ""
-    if config["build_args"]:
-        build_args = "--build-arg " + " --build-arg ".join(config["build_args"])
+    if config["build-args"]:
+        build_args = "--build-arg " + " --build-arg ".join(config["build-args"])
 
     pull_stub: str = ""
-    if config["always_pull"]:
+    if config["always-pull"]:
         pull_stub = "--pull"
 
     push_steps: List[str] = [
         'echo "Not pushing to ECR as branch not listed in push-branches"'
     ]
-    if config["push_to_ecr"]:
+    if config["push-to-ecr"]:
         push_steps = [f"docker image push {platform_image}"]
 
     composer_cache_stub: str = ""
-    if config["composer_cache"]:
+    if config["composer-cache"]:
         composer_cache_stub = "--build-context composer-cache=.composer-cache"
 
     npm_cache_stub: str = ""
-    if config["npm_cache"]:
+    if config["npm-cache"]:
         npm_cache_stub = "--build-context npm-cache=.npm-cache"
 
     yarn_cache_stub: str = ""
-    if config["yarn_cache"]:
+    if config["yarn-cache"]:
         yarn_cache_stub = "--build-context yarn-cache=.yarn-cache"
 
     scan_steps: List[str] = []
-    if config["scan_image"]:
+    if config["scan-image"]:
         scan_steps = [
             "wizcli auth --id $$WIZ_CLIENT_ID --secret $$WIZ_CLIENT_SECRET",
             f'wizcli docker scan --image {platform_image} -p "Container Scanning" -p "Secret Scanning" --tag pipeline={os.environ["BUILDKITE_PIPELINE_NAME"]} --tag architecture={platform} --tag pipeline_run={os.environ["BUILDKITE_BUILD_NUMBER"]} > out 2>&1 | true; SCAN_STATUS=$${{PIPESTATUS[0]}}',
             # pylint: disable=anomalous-backslash-in-string
-            f'if [[ ! $$SCAN_STATUS -eq 0 ]]; then echo -e "**Container scan report [{config["image_name"]}:{config["image_tag"]}] ({platform})**\n\n<details><summary></summary>\n\n\`\`\`term\n$(cat out**)\`\`\`\n\n</details>" | buildkite-agent annotate --style error --context {"".join(item for item in config["image_name"] if item.isalnum())}-{"".join(item for item in config["image_tag"] if item.isalnum())}-{platform}-security-scan; fi',
+            f'if [[ ! $$SCAN_STATUS -eq 0 ]]; then echo -e "**Container scan report [{config["image-name"]}:{config["image-tag"]}] ({platform})**\n\n<details><summary></summary>\n\n\`\`\`term\n$(cat out**)\`\`\`\n\n</details>" | buildkite-agent annotate --style error --context {"".join(item for item in config["image-name"] if item.isalnum())}-{"".join(item for item in config["image-tag"] if item.isalnum())}-{platform}-security-scan; fi',
         ]
         if BLOCK_ON_CONTAINER_SCAN:
             scan_steps.append(
@@ -223,15 +218,15 @@ def create_build_step(
 
     step_label = (
         f":docker: Build and push {platform} image"
-        if config["push_to_ecr"]
+        if config["push-to-ecr"]
         else f":docker: Build {platform} image"
     )
     step = {
         "label": step_label,
-        "key": f'{config["group_key"]}-build-push-{platform}',
+        "key": f'{config["group-key"]}-build-push-{platform}',
         "command": [
             f"docker buildx use builder || docker buildx create --bootstrap --name builder --use --driver docker-container --driver-opt image=moby/buildkit:{BUILDKIT_VERSION}",
-            f'docker buildx build --load {pull_stub} --ssh default {cache_from_images_stub} {build_args} {composer_cache_stub} {npm_cache_stub} {yarn_cache_stub} --tag {platform_image} -f {config["dockerfile_path"]} {config["context_path"]}',
+            f'docker buildx build --load {pull_stub} --ssh default {cache_from_images_stub} {build_args} {composer_cache_stub} {npm_cache_stub} {yarn_cache_stub} --tag {platform_image} -f {config["dockerfile-path"]} {config["context-path"]}',
             *scan_steps,
             *push_steps,
         ],
@@ -248,7 +243,7 @@ def create_build_step(
         ],
     }
 
-    if config["composer_cache"]:
+    if config["composer-cache"]:
         step["command"].insert(0, "mkdir -p .composer-cache")
         step["command"].insert(0, 'echo ".composer-cache" >> .dockerignore')
         step["plugins"].append(
@@ -263,7 +258,7 @@ def create_build_step(
             }
         )
 
-    if config["npm_cache"]:
+    if config["npm-cache"]:
         step["command"].insert(0, "mkdir -p .npm-cache")
         step["command"].insert(0, 'echo ".npm-cache" >> .dockerignore')
         step["plugins"].append(
@@ -278,7 +273,7 @@ def create_build_step(
             }
         )
 
-    if config["yarn_cache"]:
+    if config["yarn-cache"]:
         step["command"].insert(0, "mkdir -p .yarn-cache")
         step["command"].insert(0, 'echo ".yarn-cache" >> .dockerignore')
         step["plugins"].append(
@@ -299,49 +294,49 @@ def create_build_step(
 def create_oci_manifest_step(config: Dict[str, Any]) -> Dict[str, Any]:
     """Create a step stub to create a container manifest and push it to ECR"""
     images: List[str] = [
-        f'{config["fully_qualified_image_name"]}:multi-platform-{config["image_tag"]}-{platform}'
+        f'{config["fully-qualified-image-name"]}:multi-platform-{config["image-tag"]}-{platform}'
         for platform, _ in BUILD_PLATFORMS.items()
-        if config[f"build_{platform}"]
+        if config[f"build-{platform}"]
     ]
     dependencies: List[str] = [
-        f'{config["group_key"]}-build-push-{platform}'
+        f'{config["group-key"]}-build-push-{platform}'
         for platform, _ in BUILD_PLATFORMS.items()
-        if config[f"build_{platform}"]
+        if config[f"build-{platform}"]
     ]
 
     basic_actions = [
-        f'docker buildx imagetools create -t {config["fully_qualified_image_name"]}:{config["image_tag"]} {" ".join(images)}',
+        f'docker buildx imagetools create -t {config["fully-qualified-image-name"]}:{config["image-tag"]} {" ".join(images)}',
     ]
-    if config["mutate_image_tag"]:
+    if config["mutate-image-tag"]:
         basic_actions.insert(
             0,
-            f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository_namespace"]}/{config["image_name"]} --image-ids imageTag={config["image_tag"]} || true',
+            f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository-namespace"]}/{config["image-name"]} --image-ids imageTag={config["image-tag"]} || true',
         )
 
     step = {
         "label": ":docker: Create container manifest",
         "depends_on": dependencies,
-        "key": f'{config["group_key"]}-manifest',
+        "key": f'{config["group-key"]}-manifest',
         "command": basic_actions,
         "plugins": [],
     }
 
-    if config["additional_tag"]:
-        if config["mutate_image_tag"]:
+    if config["additional-tag"]:
+        if config["mutate-image-tag"]:
             step["command"].append(
-                f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository_namespace"]}/{config["image_name"]} --image-ids imageTag={config["additional_tag"]} || true'
+                f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository-namespace"]}/{config["image-name"]} --image-ids imageTag={config["additional-tag"]} || true'
             )
         step["command"].append(
-            f'docker buildx imagetools create -t {config["fully_qualified_image_name"]}:{config["additional_tag"]} {" ".join(images)}'
+            f'docker buildx imagetools create -t {config["fully-qualified-image-name"]}:{config["additional-tag"]} {" ".join(images)}'
         )
 
     if CURRENT_BRANCH != "":
         # Always remove the cache_branch tagged image so we can update it in immutable repositories as cache for the next build
         step["command"].append(
-            f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository_namespace"]}/{config["image_name"]} --image-ids imageTag=cache_{CURRENT_BRANCH} || true'
+            f'aws ecr batch-delete-image --registry-id {ECR_ACCOUNT} --repository-name {config["repository-namespace"]}/{config["image-name"]} --image-ids imageTag=cache_{CURRENT_BRANCH} || true'
         )
         step["command"].append(
-            f'docker buildx imagetools create -t {config["fully_qualified_image_name"]}:cache_{CURRENT_BRANCH} {" ".join(images)}'
+            f'docker buildx imagetools create -t {config["fully-qualified-image-name"]}:cache_{CURRENT_BRANCH} {" ".join(images)}'
         )
 
     return step
@@ -349,25 +344,25 @@ def create_oci_manifest_step(config: Dict[str, Any]) -> Dict[str, Any]:
 
 def main():
     """Generate and output to stdout a pipeline for building, pushing and scanning a multi-platform container image."""
-    config = process_env_to_config()
+    config = process_config()
 
     pipeline = {}
     pipeline["steps"] = []
     pipeline["steps"].append(
         {
             "group": ":docker: Build and push images",
-            "key": config["group_key"],
+            "key": config["group-key"],
             "steps": [],
         }
     )
 
     for platform, agent in BUILD_PLATFORMS.items():
-        if config[f"build_{platform}"]:
+        if config[f"build-{platform}"]:
             pipeline["steps"][0]["steps"].append(
                 create_build_step(platform, agent, config)
             )
 
-    if config["push_to_ecr"]:
+    if config["push-to-ecr"]:
         pipeline["steps"][0]["steps"].append(create_oci_manifest_step(config))
 
     with open("pipeline.yaml", "w", encoding="utf8") as file:
